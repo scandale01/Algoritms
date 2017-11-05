82e25a5bb47276f4c27736cea6642f43683f6c21
scandale01 <scandale89@gmail.com>
2017-11-05 22:28:19 +0200

    Up to 3 task is complite.

--- Dz_2.c
+++ Dz_2.c
@@ -1,4 +1,7 @@
-﻿#include <stdio.h>
+﻿/*Homework number 2, Algoritms and data structures.
+Student: Vyacheslav Serov
+*/
+#include <stdio.h>
 /*
 int G(int n);
 int F(int n) {
@@ -31,9 +34,69 @@ int G(int n) {
 		return sumDigit(a/10) + a%10;
 	}
 }*/
+void recursionTenToDouble (int n) {
+	if (n)
+	{
+		int b = n % 2;
+		recursionTenToDouble(n /= 2);
+		printf("%d", b);
+	}
+}
 
+/*1. Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.*/
+void fromTentoDoubleSystem() {
+	int number;
+	printf("Enter number for convertion\n");
+	scanf("%d", &number);
+	recursionTenToDouble(number);
+	return 0;
+}
+
+/*Реализовать функцию возведения числа a в степень b:
+
+a. без рекурсии;
+b. рекурсивно;
+c. *рекурсивно, используя свойство чётности степени.*/
+int inPowerA(int num, int stepen) {
+	int n, s, k;
+	n = 1;
+	s = num;
+	k = stepen;
+	while (k != 0)
+	{
+		if ((k % 2) == 0)
+		{
+			s *= s;
+			k = k / 2;
+		}
+		else
+		{
+			k--;
+			n *= s;
+		}
+	}
+	return n;
+}
+int inPowerB(int num, int stepen) {
+	if (stepen == 0)
+	{
+		return 1;
+	}
+	
+	return num * inPowerB(num, stepen-1);
+}
+void inPower() {
+	int a, b;
+	printf("Enter number\n");
+	scanf("%d", &a);
+	printf("Enter number power number\n");
+	scanf("%d", &b);
+	printf("Number: %d\n", inPowerB(a, b));
+}
 int main(int argc, const char *argv[]) {
 	//F(6);
-	sumDigit(23);
+	//sumDigit(23);
+	//fromTentoDoubleSystem();
+	inPower();
 	return 0;
 }
